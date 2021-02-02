16a59964ff2f94d5cb840301bb9acd92b85a8151
CanadaBry <5093727+CanadaBry@users.noreply.github.com>
2021-02-02 17:42:59 -0500

    Removed read and replaced with waits Using 2 waits since PID changes status when shutting down and then when exiting

--- server.sh
+++ server.sh
@@ -18,9 +18,9 @@ SERVER_PASSWORD=${SERVER_PASSWORD:-secret}
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
-rm -f /home/steam/server_data/server_exit.drp
 trap "echo 1 > server_exit.drp;exit 1" SIGTERM
 
 /home/steam/server_data/valheim_server.x86_64 -name "$SERVER_NAME" -port $SERVER_PORT -world "$SERVER_WORLD" -password "$SERVER_PASSWORD" -public 1 &
 
-read;
+wait $!
+wait $!
